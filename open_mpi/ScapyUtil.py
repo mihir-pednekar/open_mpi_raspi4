160b8458eb3e44afa43ed7d0dcd82d3d7fcc6974
papajones03 <papajones03@gmail.com>
2020-06-04 02:07:39 +0100

    Scapy python code to read pcap

--- open_mpi/ScapyUtil.py
+++ open_mpi/ScapyUtil.py
@@ -1,32 +1,7 @@
-'''
-Created on 03-Jun-2020
-
-@author: mihir
-'''
-
 from scapy.all import *
 
 class ScapyUtil:
-    '''
-    Too read pcap files
-    '''
-    pcap_file_name = ""
 
-    def __init__(self, path):
-        '''
-        Constructor
-        '''
-        self.pcap_file_name = path
-        
-    def read_pcap(self):
-        # rdpcap comes from scapy and loads in our pcap file
-        packets = rdpcap('example.pcap')
-        
-        # Let's iterate through every packet
-        for packet in packets:
-            # We're only interested packets with a DNS Round Robin layer
-            if packet.haslayer(DNSRR):
-                # If the an(swer) is a DNSRR, print the name it replied with.
-                if isinstance(packet.an, DNSRR):
-                    print(packet.an.rrname)
-        
+	def read_pcap(self, path):
+		packets = rdpcap(path)
+		print(packets[0].show())
---
+++ open_mpi/main.py
@@ -0,0 +1,6 @@
+from ScapyUtil import ScapyUtil
+
+scapy = ScapyUtil()
+
+scapy.read_pcap("example.pcap")
+
