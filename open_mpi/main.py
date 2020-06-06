64063a6a6633bc6889dde7bbd5a742c33db2780c
papajones03 <papajones03@gmail.com>
2020-06-06 01:31:05 +0100

    Error made changes in previous

--- open_mpi/main.py
+++ open_mpi/main.py
@@ -5,8 +5,7 @@ packets = scapy.read_pcap("example.pcap")
 
 for pkt in packets:
     new_scapy = ScapyUtil()
-    for i in range(0,5):
-        file_name = "pkt_"+str(new_scapy.get_timestamp())+".pcap"
-        print(file_name)
-        new_scapy.write_pcap(pkt, file_name)
+    file_name = "pkt_"+str(new_scapy.get_timestamp())+".pcap"
+    print(file_name)
+    new_scapy.write_pcap(pkt, file_name)
     
