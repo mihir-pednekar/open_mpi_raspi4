e296360bc85d5e04163199ce36919cc56fcff88a
papajones03 <papajones03@gmail.com>
2020-06-06 01:19:00 +0100

    Error : no ref to scapy

--- open_mpi/main.py
+++ open_mpi/main.py
@@ -1,11 +1,11 @@
 from ScapyUtil import ScapyUtil
 
+scapy = ScapyUtil()
 packets = scapy.read_pcap("example.pcap")
 
 for i in range(0,5):
     for pkt in packets:
-        scapy = ScapyUtil()
-        file_name = "pkt_"+str(scapy.get_timestamp())
+        file_name = "pkt_"+str(ScapyUtil().get_timestamp())
         print(file_name)
         scapy.write_pcap(pkt, file_name)
     
