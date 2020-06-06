8338ce38e6d8edfb61a7d6a18383c4e830d6fcdd
papajones03 <papajones03@gmail.com>
2020-06-06 12:00:37 +0100

    Changes for timestamp

--- open_mpi/main.py
+++ open_mpi/main.py
@@ -3,15 +3,9 @@ from ScapyUtil import ScapyUtil
 scapy = ScapyUtil()
 packets = scapy.read_pcap("example.pcap")
 
-time_lst = []
-
 for pkt in packets:
     new_scapy = ScapyUtil()
-    time_lst.append(new_scapy)
-    
-for pkt in packets:
-    for i in range(5):
-        file_name = "pkt_"+str(time_lst[i].get_timestamp())+".pcap"
-        print(file_name)
-        new_scapy.write_pcap(pkt, file_name)
+    file_name = "pkt_"+str(new_scapy.get_timestamp())+".pcap"
+    print(file_name)
+    new_scapy.write_pcap(pkt, file_name)
     
