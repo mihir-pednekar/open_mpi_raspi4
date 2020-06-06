cb3ff4ac5389744f603a7213a89bc04c993e3d53
papajones03 <papajones03@gmail.com>
2020-06-06 12:04:30 +0100

    Iteration changes

--- open_mpi/main.py
+++ open_mpi/main.py
@@ -2,10 +2,12 @@ from ScapyUtil import ScapyUtil
 
 scapy = ScapyUtil()
 packets = scapy.read_pcap("example.pcap")
+i = 0
 
 for pkt in packets:
     new_scapy = ScapyUtil()
-    file_name = "pkt_"+str(new_scapy.get_timestamp())+".pcap"
+    file_name = "pkt_"+str(new_scapy.get_timestamp()+i)+".pcap"
+    i += 1
     print(file_name)
     new_scapy.write_pcap(pkt, file_name)
     
