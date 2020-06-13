3b5a2446fd7ab97b37f400d4e1221d9224b26044
papajones03 <papajones03@gmail.com>
2020-06-13 17:59:56 +0100

    Cast to String

--- open_mpi/cluster.py
+++ open_mpi/cluster.py
@@ -1,7 +1,9 @@
 from mpi4py import MPI
 from ScapyUtil import ScapyUtil
 from main import FileList
+import datetime as dt
 
+n1=dt.datetime.now()
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
@@ -17,8 +19,13 @@ else:
 scatter_lst = comm.scatter(file_lst, root=0)
 print("<================ RANK "+str(rank)+" ==================>")
 print(scatter_lst)
-
+print("RANK "+str(rank)+" time : "+str(n1.microsecond)+" ms.")
 for pcap_file in scatter_lst:
-    pkt = ScapyUtil().read_pcap(pcap_file)
-    print(pkt[0].summary())
+    #pkt = ScapyUtil().read_pcap(pcap_file)
+    print(pcap_file)
+
+n2=dt.datetime.now()
+(n2-n1).microseconds
+(n2.microsecond-n1.microsecond)/1e6
+
 print("<================ RANK "+str(rank)+" ==================>")
--- open_mpi/main.py
+++ open_mpi/main.py
@@ -11,7 +11,7 @@ class FileList:
         for pkt in packets:
             new_scapy = ScapyUtil()
             file_name = "pkt_"+str(int(new_scapy.get_timestamp())+i)+".pcap"
-            file_lst.append(file_name) 
+            file_lst.append(str(pkt.summary())) 
             i += 1
             print(file_name)
             new_scapy.write_pcap(pkt, file_name)
