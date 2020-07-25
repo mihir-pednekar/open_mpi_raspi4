b0336019bc56e32c56f3f6974c51cfbbfc2ecc54
papajones03 <papajones03@gmail.com>
2020-07-25 18:23:20 +0100

    RuleSet4

--- open_mpi/cluster.py
+++ open_mpi/cluster.py
@@ -27,9 +27,6 @@ else:
    rules = file_list_obj.read_rule(rule_file_name)
    list_rule_map = file_list_obj.map_of_rules(rules)
    file_lst, pkts, p_len = file_list_obj.list_of_file(size, pcap_file_name)
-   for pkt in pkts:
-       for field in pkt['Ethernet'].fields_desc:
-           print(field.name)
    
 scatter_lst = comm.scatter(file_lst, root=0)
 
