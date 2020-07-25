b4f2d2ebdfd1b513f11878858c7b748a2637ddbb
papajones03 <papajones03@gmail.com>
2020-07-25 22:25:56 +0100

    Test1

--- open_mpi/cluster.py
+++ open_mpi/cluster.py
@@ -16,16 +16,16 @@ max_index = None
 pcap_file_name = sys.argv[1]
 rule_file_name = sys.argv[2]
 file_list_obj = FileList()
-list_rule_map = None
+rule_map = None
 
 if rank == 0:
    rules = file_list_obj.read_rule(rule_file_name)
-   list_rule_map = file_list_obj.map_of_rules(rules)
+   rule_map = file_list_obj.map_of_rules(rules)
    file_lst, pkts, p_len = file_list_obj.list_of_file(size, pcap_file_name)
     
 else:
    rules = file_list_obj.read_rule(rule_file_name)
-   list_rule_map = file_list_obj.map_of_rules(rules)
+   rule_map = file_list_obj.map_of_rules(rules)
    file_lst, pkts, p_len = file_list_obj.list_of_file(size, pcap_file_name)
    
 scatter_lst = comm.scatter(file_lst, root=0)
@@ -34,26 +34,22 @@ scatter_lst = comm.scatter(file_lst, root=0)
 
 for scattr in scatter_lst:
     scat_list = scattr.split(':', 1)
-    min_index = scat_list[0]
-    max_index = scat_list[1]
+    min_index = int(scat_list[0])
+    max_index = int(scat_list[1])
 
 print("min : "+min_index)
 print("max : "+max_index)
 #traverse list of packets
 proto_map = {}
 for itr in range(0, p_len):
-    if pkts[itr].haslayer('Ethernet'):
-        print("haslayer(Ethernet)")
+    for protocol in rule_map:
+        if len(rule_map[protocol]) != 0:
+            if pkts[itr].haslayer(protocol):
+                print("haslayer("+protocol+")")
+                for field in pkts[itr][protocol].fields_desc:
+                    proto_map[itr][protocol][field] = getattr(pkts[itr][protocol], field)
            
-    if pkts[itr].haslayer('IP'):
-        print("haslayer(IP)")
-        
-    if pkts[itr].haslayer('TCP'):
-        print("haslayer(TCP)")
-           
-for pkt in pkts:
-    for field in pkt['Ethernet'].fields_desc:
-        print(field.name)
+print(proto_map)
 
 n2=dt.datetime.now()
 print("<================ RANK "+str(rank)+" TIME DIFF : "+str(((n2-n1).microseconds)/1000)+" ms. ================>")
